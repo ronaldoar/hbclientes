0b2dbdeb4bc6456e5523e1984542940e3ef3a3bc
= <ronaldo.alves.rios@gmail.com>
2022-03-25 14:51:29 -0300

    Artefatos iniciais do projeto

---
+++ src/main/java/br/com/cs/hbclientes/HbeerClientesApplication.java
@@ -0,0 +1,13 @@
+package br.com.cs.hbclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbeerClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbeerClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cs/hbclientes/HbeerClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cs.hbclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbeerClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
